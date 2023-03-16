025410805baf6a7db403d4afc3a6b89f109fd4cc
KoftaBozo2235 <omarehab2217@gmail.com>
2023-03-16 11:58:40 -0700

    Automated Commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/**
+ *main is a thing
+ *Description: the function is a function
+ *Return: Return is a Return
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
