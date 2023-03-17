a50ae7a29e9d8400715731e4ba61dd99d9054ff0
KoftaBozo2235 <omarehab2217@gmail.com>
2023-03-17 14:29:42 -0700

    Automated Commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - main
+ * Return: return
+ */
+int main(void){
+	char hambola = 'a';
+	
+	while (hambola <='z')
+	{
+		putchar(hambola);
+		hambola++;
+	}
+	putchar('\n');
+	return (0);
+}
