dd529e3fa08a36cbe3d323db3cfa459078925f36
KoftaBozo2235 <omarehab2217@gmail.com>
2023-03-17 13:18:54 -0700

    Automated Commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,18 +11,17 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-        scanf("%d", &n);
 	if (n > 0)
 	{
-	printf("%d is positive");
+	printf("%i is positive\n", n);
 	}
 	else if (n < 0)
 	{
-	printf("%d is negative");
+	printf("%i is negative\n", n);
 	}
-	else if (n = 0)
+	else if (n == 0)
 	{
-	printf("%d is zero");
+	printf("%i is zero\n", n);
 	}
 	return (0);
 }
