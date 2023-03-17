dbffcc56d88ad921608c131d793c68d957aa2b15
KoftaBozo2235 <omarehab2217@gmail.com>
2023-03-17 15:20:53 -0700

    Automated Commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main
+ * Return: return
+ */
+int main(void)
+{
+        char hambola = 'z';
+
+        while (hambola >= 'a')
+        {
+                putchar(hambola);
+                hambola--;
+        }
+        putchar('\n');
+        return (0);
+}
---
+++ 0x01-variables_if_else_while/example.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(void)
+{
+	int n = 9;
+	printf ("n = %i\n", n);
+	return (0);
+}
