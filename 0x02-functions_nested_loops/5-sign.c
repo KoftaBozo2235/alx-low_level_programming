1d3daf5cd39ad7ab00c9fa5519f48614a5424f0b
KoftaBozo2235 <omarehab2217@gmail.com>
2023-03-21 10:54:43 -0700

    Automated Commit

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,26 +1,30 @@
 #include "main.h"
+
 /**
- * _isalpha - check for lower case letter
- * @c : character to check the case
+ * print_sign - check numers
+ * @n : number to check
  * Return:0 or 1
  */
+
 int print_sign(int n)
 {
-int value
+int value;
+
 if (n > 0)
 {
-_putchar("+");
-value = 1;
+	value = 1;
+	_putchar('+');
 }
-else if (n = 0)
+else if (n == 0)
 {
-_putchar("0");
-value = 0;
+	value = 0;
+	_putchar('0');
 }
-else if (n > 0)
+else
 {
-_putchar("-")
-value = -1;
+	value = -1;
+	_putchar('-');
 }
-return (value);
+
+return (test);
 }
