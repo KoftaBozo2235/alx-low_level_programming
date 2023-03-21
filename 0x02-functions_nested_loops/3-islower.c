867ffba0900566ac802a01de2d3166ab0d6d2852
KoftaBozo2235 <omarehab2217@gmail.com>
2023-03-21 10:11:49 -0700

    Automated Commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet 10 times
+ * Return:void
+ */
+
+int _islower(int c)
+{
+if( islower(c))
+{
+Return (1);
+}
+else
+{
+Return (0);
+}
+}
