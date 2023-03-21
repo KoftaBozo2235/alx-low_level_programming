26cf3717bb69760a181a7501c513c2ae1be0b4ad
KoftaBozo2235 <omarehab2217@gmail.com>
2023-03-21 10:42:12 -0700

    Automated Commit

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * main - check for lower case letter
+ * _isalpha - check for lower case letter
  * @c : character to check the case
  * Return:0 or 1
  */
 int _isalpha(int c)
 {
-return((c >= 65 && c <= 90) || (c >= 97 && c <= 122));
+return ((c >= 65 && c <= 90) || (c >= 97 && c <= 122));
 }
