17f7f77bd5da05c8e5e4af2bbb969c2fed5783c1
KoftaBozo2235 <omarehab2217@gmail.com>
2023-03-21 02:27:26 -0700

    Automated Commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
+#include "main.h"
 /**
  * main - main
  * Return: is a return
  */
-void print_alphabet(void)
-{
-printf("abcdefghijklmnopqrstuvwxyz\n");
-}
 int main(void)
 {
 print_alphabet();
