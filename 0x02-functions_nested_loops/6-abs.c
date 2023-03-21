5f2dfc6dec76676200fa118bd335494059c51ecf
KoftaBozo2235 <omarehab2217@gmail.com>
2023-03-21 11:03:41 -0700

    Automated Commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - check for lower case letter
+ * @c : character to check the case
+ * Return:0 or 1
+ */
+int _abs(int)
+{
+if (int < 0)
+{
+int = int * -1;
+}
+return(int);
+}
