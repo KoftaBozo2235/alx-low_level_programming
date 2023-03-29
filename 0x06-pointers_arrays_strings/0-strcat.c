afd04105f918b13ee910abbca05e9e1c49f17ed4
KoftaBozo2235 <omarehab2217@gmail.com>
2023-03-29 15:50:12 -0700

    Automated Commit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * *_strcat - concatenates two strings
+ * @dest: pointer destination
+ * @src: pointer source
+ * Return: void
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	int i;
+
+	   for(int i=0; src[j]!='\0'; j++)
+    {
+
+        dest[i]=src[j];
+        i++;
+    }
+    dest[i]='\0';
