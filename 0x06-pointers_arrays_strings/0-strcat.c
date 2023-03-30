fa3593aafb1b883f1bb575bc165365570e650553
KoftaBozo2235 <omarehab2217@gmail.com>
2023-03-29 17:07:42 -0700

    Automated Commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,18 +9,19 @@
 char *_strcat(char *dest, char *src)
 {
 	
-	char *hamdy = dest;
-	int i = 0;
-	
+char *hamdy = dest;
+int i = 0;
+int j = 0;
 	
 	while (dest[i] !='\0')
 	{
 		i++;
 	}
 	
-	while (src[i] !='\0')
+	while (src[j] !='\0')
 	{
-		dest[i] = src[i];
+		dest[i] = src[j];
+		j++;
 		i++;
 	}
 	dest[i]='\0';
