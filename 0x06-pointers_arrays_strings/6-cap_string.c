999fb3105427994983334a204d8d497a6a01f3ef
KoftaBozo2235 <omarehab2217@gmail.com>
2023-03-30 15:06:31 -0700

    Automated Commit

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,15 +8,36 @@
 
 char *cap_string(char *str)
 {
-int i;
+	char sep[] = ",\t;\n; .!?\"(){}";
+	int flag, i, ii;
 
-while (str[i] !='\0')
-{
-	if (str[i] <= 97 && str[i] <= 122)
+	for (i = 0; str[i] != '\0'; i++)
 	{
-	str[i] = str[i] - 32;
+		flag = 0;
+
+		if (i == 0)
+		{
+			flag = 1;
+		}
+		else
+		{
+			for (ii = 0; sep[ii] != '\0'; ii++)
+			{
+				if (str[i - 1] == sep[ii])
+				{
+					flag = 1;
+					break;
+				}
+			}
+		}
+
+		if (flag == 1)
+		{
+			if (str[i] <= 'z' && str[i] >= 'a')
+			{
+				str[i] -= ('a' - 'A');
+			}
+		}
 	}
-i++;
-}
-return(str);
+	return (str);
 }
