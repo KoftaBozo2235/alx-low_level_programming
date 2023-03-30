b3caa543a471e29d5bf113f401ecedf7943e8bf4
KoftaBozo2235 <omarehab2217@gmail.com>
2023-03-30 10:25:47 -0700

    Automated Commit

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -8,7 +8,7 @@
 char *string_toupper(char *str)
 {
 int i = 0;
-while (str[i] !='\0')
+while (str[i] != '\0')
 {
 	if (str[i] >= 97 && str[i] <= 122)
 	{
@@ -16,5 +16,5 @@ while (str[i] !='\0')
 	}
 i++;
 }
-return (0);
+return (str);
 }
