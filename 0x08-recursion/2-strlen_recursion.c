b087c40fd0bfebf11d97536018ca6bfc83a6f8d3
KoftaBozo2235 <omarehab2217@gmail.com>
2023-04-05 16:30:33 -0700

    Automated Commit

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,12 +8,12 @@
 
 int _strlen_recursion(char *s)
 {
-	int i = 0;
-	
-	if (*s > '\0')
-	{
-	 	i += _strlen_recursion(s + 1) + 1;
-	
-	}
+int i = 0;
+
+if (*s > '\0')
+{
+i += _strlen_recursion(s + 1) + 1;
+
+}
 return (i);
 }
