c6a5643712f9dcf3f59ebac675d8b353327db0b3
KoftaBozo2235 <omarehab2217@gmail.com>
2023-04-07 12:53:15 -0700

    Automated Commit

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,5 +1,9 @@
-#ifndef MAIN_H
-#define MAIN_H
+
+#ifndef FILE_MAIN
+#define FILE_MAIN
+
+/*copy list from task*/
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -20,4 +24,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+
 #endif
