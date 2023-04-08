f27e8c6b5e95133dd0838ef974ddf8a4c7aaa569
KoftaBozo2235 <omarehab2217@gmail.com>
2023-04-08 16:20:20 -0700

    Automated Commit

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,6 +8,15 @@
  */
 int main(int argc, char *argv[] __attribute__((unused)))
 {
-printf("%i\n", argc);
+
+if (argc > 0)
+{
+	printf("%i\n", argc -1);
+}
+else
+{
+	printf("0\n");
+}
+
 return (0);
 }
