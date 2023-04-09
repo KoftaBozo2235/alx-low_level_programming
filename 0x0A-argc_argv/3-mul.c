57d12a485f0382a2328b8a4d1a4c017cbb66947c
KoftaBozo2235 <omarehab2217@gmail.com>
2023-04-08 17:10:49 -0700

    Automated Commit

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two numbers
+ * @argc: n args
+ * @argv: arr args
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i = 1;
+	int sum;
+
+	for (i = 1; i < argc; i++)
+	{
+		sum *= atoi(argv[i]);
+	}
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+printf("%d\n", sum);
+return (0);
+}
