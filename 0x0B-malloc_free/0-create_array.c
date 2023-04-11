add8741e350f94e6f22bcb722928bb728233f8d4
KoftaBozo2235 <omarehab2217@gmail.com>
2023-04-11 07:32:21 -0700

    Automated Push

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ *create_array - array for prints a string
+ *@size: number elements array
+ *@c: char
+ *Return: pointer
+ */
+
+char *create_array(unsigned int size, char c)
+{
+char *thing;
+unsigned int i;
+
+if (size == 0)
+{
+	return (NULL);
+}
+
+thing = (char *) malloc(size * sizeof(c));
+
+if (thing = 0)
+{
+	return (NULL);
+}
+else
+	{
+i = 0;
+while (i < size)
+{
+*(thing + i) = c;
+i++;
+}
+
+return (buffer);
+}
