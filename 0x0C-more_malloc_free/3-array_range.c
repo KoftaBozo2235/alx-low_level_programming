ca2e68042913640a5f80825d2e47e3505d8b7d39
KoftaBozo2235 <omarehab2217@gmail.com>
2023-04-13 15:19:50 -0700

    Automated Commit

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -11,21 +11,17 @@ int *array_range(int min, int max)
 	int *arr;
 	int i = 0;
 	int j = min;
-
-	arr = malloc((min - max) * sizeof(int));
-	if (!arr)
-	{
-                return (0);
-	}
 	if (min > max)
 	{
 		return (0);
 	}
-	while (i <= max - min)
+	arr = malloc((max - min + 1) * sizeof(int));
+	if (!arr)
 	{
-		arr[i] = arr[j];
-		i++;
-		j++;
+		return (0);
 	}
+
+	while (i <= max - min)
+		arr[i++] = j++;
 	return (arr);
 }	
