907fc40d2eb4f40c48fcdce0a4f26a4a86916aef
KoftaBozo2235 <omarehab2217@gmail.com>
2023-04-17 14:35:35 -0700

    Automated Commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,16 @@
 #ifndef _DOG
 #define _DOG
 
+/**
+ * struct dog - structure dog
+ * @name: atribute pointer to char name's dog
+ * @age: atribute float age's dog
+ * @owner: atribute pointer to char owner's dog
+ */
+
 struct dog
 {
-	char  *name;
+	char *name;
 	float age;
-        char* owner;
+	char *owner;
 };
-#endif
