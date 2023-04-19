bb47f895b9b6d4f539ebb741bb88aac5f0766c79
KoftaBozo2235 <omarehab2217@gmail.com>
2023-04-19 16:36:02 -0700

    Automated Commit

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,10 +12,12 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i = 0;
-
+	if (array && size && action)
+	{
 	while (i < size)
 	{
 		action(array[i]);
 		i++;
+	}
 	}
 }
